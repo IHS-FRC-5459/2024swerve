83922489f519ec5a0c34f0c1c29189162eff2b1d
TobyFlowers <tobiasgflowers@gmail.com>
2023-09-30 08:13:03 -0700

    eee

--- src/main/java/frc/lib/util/LimelightCalc.java
+++ src/main/java/frc/lib/util/LimelightCalc.java
@@ -1,9 +1,13 @@
 package frc.lib.util;
 
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import frc.robot.Constants;
 
 public class LimelightCalc {
+    /* Calculations For Distances/Angles*/
+
+    //get distance to target
     public double getDistance(NetworkTableEntry ty){
         double targetAngle = ty.getDouble(0.0);
         double angleToGoalDegrees = targetAngle + Constants.Limelight.cameraAngle;
@@ -14,6 +18,43 @@ public class LimelightCalc {
         return distanceFromLimelightToGoalMeters;
         
     }
+
+    //get how far off from target center in strafing direction
+    public double getXOffset(NetworkTableEntry tx){
+        return tx.getDouble(0.0);
+    }
+
+    //get angle from target in turning direction
+    public double getTagAngle(NetworkTableEntry targetpose_cameraspace){
+       // return targetpose_cameraspace.getDoubleArray(null) FIND HOW TO GET EULER ANGLE OF APRILTAG IN CAMERA FROM :)))))
+       return 0;
+    }
+
+    /* Power Calculations */
+    public double forwardPower(double goalDistance, double currentDistance){
+        double kp = 0.1;
+        double error = currentDistance - goalDistance;
+        double output = MathUtil.clamp(kp * error, -1, 1);
+        return output;
+    }
+
+    public double strafePower(double goalStrafe, double currentStrafe){
+        double kp = 0.1;
+        double error = currentStrafe - goalStrafe;
+        double output = MathUtil.clamp(kp * error, -1, 1);
+        return output;
+
+
+    }
+
+    public double rotPower(double goalRotation, double currentRotation)
+    {
+        double kp = 0.1;
+        double error = currentRotation -  goalRotation;
+        double output = MathUtil.clamp(kp * error, -1, 1);
+        return output;
+    }
+
 
 
 }
